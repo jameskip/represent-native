50ee6c6314191918d87a2cfc04173e853e4d4d63
jameskip <me@jameskip.com>
2021-11-16 10:09:19 -0600

    Convert senate to hook

--- components/SenateList.js
+++ components/SenateList.js
@@ -1,31 +1,45 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
 import { StyleSheet, TouchableOpacity, FlatList, Text, View } from 'react-native'
 
 const Senators = () => {
+  const [senate, setSenate] = useState('')
 
-}
-  
+  useEffect(() => {
+    const hey = async () => {
+      let response = await fetch(
+        'https://api.propublica.org/congress/v1/117/senate/members.json',
+        { headers: { 'X-API-Key': 'JSp1AQhdSIuQQssE07bf5bsDT7HTpPDVQLAda1nx'} }
+      )
+      response = await response.json()
+      setSenate(response.results[0].members)
+    }
+    hey()
+  }, [])
 
-const Senators = () => {
-  constructor (props) {
-    super(props)
-    this.state = { senate: '' }
+  let isRefreshing = true
+
+  if (senate.length > 1) {
+    isRefreshing = false
   }
 
-  componentDidMount () {
-    return fetch(
-      'https://api.propublica.org/congress/v1/116/senate/members.json',
-      { headers: { 'X-API-Key': 'JSp1AQhdSIuQQssE07bf5bsDT7HTpPDVQLAda1nx' } }
-    )
-      .then(senate => senate.json())
-      .then(senateJson => {
-        this.setState({ senate: senateJson.results[0].members })
-      })
-      .catch(error => {
-        console.error(error)
-      })
-  }
+  return (
+    <View style={styles.container}>
+      <Text style={styles.header}>Senate</Text>
+      <FlatList keyExtractor={item => item.id} data={senate} refreshing={isRefreshing}
+        renderItem={({ item }) =>
+          <TouchableOpacity style={styles.item} onPress={() => this.props.navigation.navigate('Profile', { item: item })}>
 
+            <Text style={styles.name}>
+              {`${item.first_name} ${item.last_name}`}
+              <Text style={styles.party}>{`    ${item.party}`}</Text>
+            </Text>
+
+            <Text style={styles.subtext}>{`${item.title}`}</Text>
+          </TouchableOpacity>
+        }
+      />
+    </View>
+  )
 }
 
 export default Senators
